39816e540e36ed25880ed61bc5dd73c6ec6e3e70
Moses Nyongesa <nyongesamoses018@gmail.com>
2025-02-28 14:59:16 +0300

    Second commit

---
+++ vPythonIntro.py
@@ -0,0 +1,10 @@
+from vpython import *
+from time import *
+floor=box(pos=vector(0,-5,0),color=color.white,length=10,height=.1,width=10)
+ceiling=box(pos=vector(0,5,0),color=color.white,length=10,height=.1,width=10)
+wall1=box(pos=vector(5,0,0),color=color.white,length=.1,height=10,width=10)
+wall2=box(pos=vector(-5,0,0),color=color.white,length=.1,height=10,width=10)
+wall3=box(pos=vector(0,0,-5),color=color.white,length=10,height=10,width=.1)
+marble=sphere(pos=vector(0,0,0),color=color.red,radius=3)
+while True:
+    pass
