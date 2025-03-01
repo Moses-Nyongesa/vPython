cc3b465f4e1df682edededc48ace29b284767d34
Moses Nyongesa <nyongesamoses018@gmail.com>
2025-03-02 02:03:41 +0300

    Third Commit

--- vPythonIntro.py
+++ vPythonIntro.py
@@ -1,10 +1,16 @@
 from vpython import *
 from time import *
-floor=box(pos=vector(0,-5,0),color=color.white,length=10,height=.1,width=10)
-ceiling=box(pos=vector(0,5,0),color=color.white,length=10,height=.1,width=10)
-wall1=box(pos=vector(5,0,0),color=color.white,length=.1,height=10,width=10)
-wall2=box(pos=vector(-5,0,0),color=color.white,length=.1,height=10,width=10)
-wall3=box(pos=vector(0,0,-5),color=color.white,length=10,height=10,width=.1)
-marble=sphere(pos=vector(0,0,0),color=color.red,radius=3)
+floor=box(pos=vector(0,-5,0),size=vector(10,.1,10),color=color.white)
+ceiling=box(pos=vector(0,5,0),size=vector(10,.1,10),color=color.white)
+rightWall=box(pos=vector(5,0,0),size=vector(.1,10,10),color=color.white)
+leftWall=box(pos=vector(-5,0,0),size=vector(.1,10,10),color=color.white)
+backWall=box(pos=vector(0,0,-5),size=vector(10,10,.1),color=color.white)
+marble=sphere(pos=vector(0,0,0),color=color.red,radius=.75)
+deltaX=.1
+xPos=0
 while True:
-    pass
+    rate(10)
+    xPos=xPos+deltaX
+    if (xPos>5 or xPos<-5):
+        deltaX=deltaX*(-1)
+    marble.pos=vector(xPos,0,0)
