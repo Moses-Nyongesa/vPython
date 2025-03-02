e508f96fb3bdfe61de26c157fe63682a1ceec453
Moses Nyongesa <nyongesamoses018@gmail.com>
2025-03-02 10:51:07 +0300

    Fourth Commit

--- vPythonIntro.py
+++ vPythonIntro.py
@@ -1,16 +1,21 @@
 from vpython import *
 from time import *
-floor=box(pos=vector(0,-5,0),size=vector(10,.1,10),color=color.white)
-ceiling=box(pos=vector(0,5,0),size=vector(10,.1,10),color=color.white)
-rightWall=box(pos=vector(5,0,0),size=vector(.1,10,10),color=color.white)
-leftWall=box(pos=vector(-5,0,0),size=vector(.1,10,10),color=color.white)
-backWall=box(pos=vector(0,0,-5),size=vector(10,10,.1),color=color.white)
-marble=sphere(pos=vector(0,0,0),color=color.red,radius=.75)
+mRadius=.75
+wallThickniss=.1
+roomWidth=10
+roomDepth=5
+roomHeight=10
+floor=box(pos=vector(0,-roomHeight/2,0),size=vector(roomWidth,wallThickniss,roomDepth),color=color.white)
+ceiling=box(pos=vector(0,roomHeight/2,0),size=vector(roomWidth,wallThickniss,roomDepth),color=color.white)
+rightWall=box(pos=vector(roomWidth/2,0,0),size=vector(wallThickniss,roomHeight,roomDepth),color=color.white)
+leftWall=box(pos=vector(-roomWidth/2,0,0),size=vector(wallThickniss,roomHeight,roomDepth),color=color.white)
+backWall=box(pos=vector(0,0,-roomDepth/2),size=vector(roomWidth,roomHeight,wallThickniss),color=color.white)
+marble=sphere(pos=vector(0,0,0),color=color.red,radius=mRadius)
 deltaX=.1
 xPos=0
 while True:
     rate(10)
     xPos=xPos+deltaX
-    if (xPos>5 or xPos<-5):
+    if (xPos>roomWidth/2 or xPos<-roomWidth/2):
         deltaX=deltaX*(-1)
     marble.pos=vector(xPos,0,0)
