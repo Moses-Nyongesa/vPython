71ceaa30c0688576fc8c15e111e3ff7b08f39ff0
Moses Nyongesa <nyongesamoses018@gmail.com>
2025-03-02 12:19:57 +0300

    Corrected Parram

--- correctedParam.py
+++ correctedParam.py
@@ -1,10 +1,10 @@
 from vpython import *
 from time import *
-mRadius=.75
-wallThickness=.1
-roomWidth=10
-roomDepth=5
-roomHeight=10
+mRadius=2
+wallThickness=1
+roomWidth=15
+roomDepth=12
+roomHeight=8
 floor=box(pos=vector(0,-roomHeight/2,0),size=vector(roomWidth,wallThickness,roomDepth),color=color.white)
 ceiling=box(pos=vector(0,roomHeight/2,0),size=vector(roomWidth,wallThickness,roomDepth),color=color.white)
 rightWall=box(pos=vector(roomWidth/2,0,0),size=vector(wallThickness,roomHeight,roomDepth),color=color.white)
@@ -16,6 +16,10 @@ xPos=0
 while True:
     rate(10)
     xPos=xPos+deltaX
-    if (xPos>roomWidth/2 or xPos<-roomWidth/2):
+    Xrme=xPos+mRadius
+    Xlme=xPos-mRadius
+    Rwe=roomWidth/2-wallThickness/2
+    Lwe=-roomWidth/2+wallThickness/2
+    if (Xrme>=Rwe or Xlme<=Lwe):
         deltaX=deltaX*(-1)
     marble.pos=vector(xPos,0,0)
